4cd25ba5a361b271a0ca2a09167472e48a31e3b3
renacava <36764359+renacava@users.noreply.github.com>
2024-05-02 21:14:44 +0200

    add #'elo-match

--- main.lisp
+++ main.lisp
@@ -17,3 +17,11 @@
      :rating-2 (+ rating-2 (* k (if did-1-win
                                     (- probability-1-wins)
                                     (- 1 probability-1-wins)))))))
+
+(defun elo-match (winner-rating loser-rating &key (k 32))
+  (let* ((probability-1-wins (probability-a-wins winner-rating loser-rating))
+         (probability-2-wins (- 1.0 probability-1-wins)))
+
+    (list
+     :winner-rating (+ winner-rating (* k (- 1 probability-2-wins)))
+     :loser-rating (+ loser-rating (* k (- probability-1-wins))))))
