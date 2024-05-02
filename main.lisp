e56096f7dd9aed54ecf1bd2d2b7ef8ade97c0411
renacava <36764359+renacava@users.noreply.github.com>
2024-05-02 20:16:50 +0200

    clean; #'elo isn't destructive anymore (ew)

--- main.lisp
+++ main.lisp
@@ -1,24 +1,19 @@
 (in-package #:elo)
 
-
-
 (defun main ()
   nil)
 
-(defun probability (rating-1 rating-2)
-  (/ 1.0 (+ 1 (* 1.0 (expt 10 (/ (* 1.0 (- rating-1 rating-2)) 400))))))
+(defun probability-a-wins (a-rating b-rating)
+  (/ 1.0 (+ 1 (expt 10 (/ (- a-rating b-rating) 400)))))
 
 (defun elo (&key (rating-1 2400) (rating-2 2400) (k 32) (did-1-win t))
-  (let* ((probability-1-wins (probability rating-1 rating-2))
+  (let* ((probability-1-wins (probability-a-wins rating-1 rating-2))
          (probability-2-wins (- 1.0 probability-1-wins)))
-    (if did-1-win
-        (progn
-          (incf rating-1 (* k (- 1 probability-2-wins)))
-          (incf rating-2 (* k (- 0 probability-1-wins))))
-        (progn
-          (incf rating-1 (* k (- 1 probability-1-wins)))
-          (incf rating-2 (* k (- 0 probability-2-wins)))))
 
-    (format t "Updated ratings:~%rating-1 = ~a~%rating-2 = ~a" rating-1 rating-2)))
-
-;; (1.0 * 1.0) / (1 + 1.0 * Math.pow(10, (1.0 * (rating1 - rating2)) / 400))
+    (list
+     :rating-1 (+ rating-1 (* k (if did-1-win
+                                       (- 1 probability-2-wins)
+                                       (- 1 probability-1-wins))))
+     :rating-2 (+ rating-2 (* k (if did-1-win
+                                       (- probability-1-wins)
+                                       (- probability-1-wins)))))))
